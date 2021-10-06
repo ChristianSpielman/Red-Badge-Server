aa783eb4f592ffa1e8acec1fd981f9ab368f3498
ChristianSpielman <cspielman10752@gmail.com>
2021-10-05 20:34:29 -0400

    updated headers

--- middleware/headers.js
+++ middleware/headers.js
@@ -1,5 +1,5 @@
 module.exports = (req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
     res.header("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With, observe");
     res.header("Access-Control-Max-Age", "3600");
